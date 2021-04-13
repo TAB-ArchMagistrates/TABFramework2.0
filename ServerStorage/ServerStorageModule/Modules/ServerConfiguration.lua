5494361a22713302b9a1165292aaf09c574cfe61
Midovah <80129104+Midovah@users.noreply.github.com>
2021-04-13 10:59:16 -0400

    Quick commit for Armor

--- ServerStorage/ServerStorageModule/Modules/ServerConfiguration.lua
+++ ServerStorage/ServerStorageModule/Modules/ServerConfiguration.lua
@@ -12,12 +12,17 @@ module.DiscordTokens = {
 	Sentinels = {
 		Arrest = "https://discord.com/api/webhooks/830921472969146396/ZVn-wgfDMXHx9BxiWh-47hinI0cVFgkwdHK66LW6tLBmLvJczYWdaMFhuym2Z11b_XSg",
 		Release = "https://discord.com/api/webhooks/830922924159926323/1yaTW0Pvlr6iRN9TSJDwHeRisiPE2cBQcwgGTWKwHg2xkf5IgFfsQFqiMUlv3pDLFlgm",
-		
+
 	},
 	TAB = {
 		Exploit = "https://discord.com/api/webhooks/830921735187726426/eDkVbEd-DE87CSoO03UgZqIqcrfns8cEp5L0qrN_l2BUWYM3MdMw6ZWuPqZ8YTxl3D1J",
 		Notifications = "https://discord.com/api/webhooks/830922800248258560/h3I2A9MRboD0CBZ0snILqmAuw3MK_L_zIlFUugFxadZHogZXzhoOMjpBG2OHVOzwpN9p",
 
+	}
+}
+module.TrelloRootTokens = {
+	TAB = {
+		
 	}
 }
 return module
